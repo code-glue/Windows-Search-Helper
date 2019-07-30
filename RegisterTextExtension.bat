d6267cf424c36133adb8c5ff919d24104597d514
skataben <skataben@gmail.com>
2019-07-30 13:45:09 -0700

    Initial commit

---
+++ RegisterTextExtension.bat
@@ -0,0 +1,207 @@
+@echo off
+
+SetLocal EnableDelayedExpansion
+
+set ExitCode=1
+set ThisFileName=%~nx0
+set ThisFileNameNoExt=%~n0
+set Arg1=%1
+
+REM echo.DEBUG ExitCode='%ExitCode%'
+REM echo.DEBUG ThisFileName='%ThisFileName%'
+REM echo.DEBUG ThisFileNameNoExt='%ThisFileNameNoExt%'
+REM echo.DEBUG Arg1='%Arg1%'
+
+if not defined Arg1 goto NoArgs
+
+set Arg1NoQuotes=%Arg1:"=%
+REM echo.DEBUG Arg1NoQuotes='%Arg1NoQuotes%'
+
+if not defined Arg1NoQuotes call :BadArg & goto ExitPause
+
+set Arg2=%2
+set Arg1NoSpaces=%Arg1NoQuotes: =%
+set Extension=.%Arg1NoQuotes%
+
+REM echo.DEBUG Arg2='%Arg2%'
+REM echo.DEBUG Arg1NoSpaces='%Arg1NoSpaces%'
+REM echo.DEBUG Extension='%Extension%'
+
+if "%Arg1NoSpaces%" == "/?" goto HelpArg
+if defined Arg2 goto TooManyArgs
+
+goto GetExtension
+
+
+:NoArgs
+REM echo.DEBUG :NoArgs %*
+call :PrintHeader
+
+
+:UserEnterExtension
+REM Prompt the user for the file extension. On error, reset and try again.
+call :SetErrorLevel 0
+set /p Arg1="Enter file extension [Ctrl+C to exit]: "
+if %ErrorLevel% neq 0 set "Arg1=" & goto UserEnterExtension
+
+REM echo.DEBUG Arg1='%Arg1%'
+if not defined Arg1 goto UserEnterExtension
+
+set Arg1NoSpaces=%Arg1: =%
+REM echo.DEBUG Arg1NoSpaces='%Arg1NoSpaces%'
+if not defined Arg1NoSpaces goto UserEnterExtension
+
+call :Trim Arg1Trimmed %Arg1%
+REM echo.DEBUG Arg1Trimmed='%Arg1Trimmed%'
+
+set Arg1NoQuotes=%Arg1Trimmed:"=%
+REM echo.DEBUG Arg1NoQuotes='%Arg1NoQuotes%'
+if not defined Arg1NoQuotes call :BadArg & goto ExitPause
+
+set Extension=.%Arg1NoQuotes%
+REM echo.DEBUG Extension='%Extension%'
+
+goto GetExtension
+
+
+:GetExtension
+REM echo.DEBUG :GetExtension
+REM echo.DEBUG Extension='%Extension%'
+
+for /f "tokens=*" %%a in ("!Extension!") do (
+    set "Extension=%%~xa"
+)
+
+REM echo.DEBUG Extension='%Extension%'
+
+if not defined Extension call :BadArg & goto ExitPause
+if "%Extension%" == "." call :BadArg & goto ExitPause
+
+goto DoSomethingWithExtension
+
+
+:DoSomethingWithExtension
+REM echo.DEBUG :DoSomethingWithExtension %*
+
+set RegKeyHKCR=HKCR\%Extension%\PersistentHandler
+set RegKeyHKCU=HKCU\Software\Classes\%Extension%\PersistentHandler
+set TextPersistentHandler={5e941d80-bf96-11cd-b579-08002b30bfeb}
+set OriginalPersistentHandlerExists=0
+set CurrentPersistentHandler=
+
+REM echo.DEBUG RegKeyHKCR='%RegKeyHKCR%'
+REM echo.DEBUG RegKeyHKCU='%RegKeyHKCU%'
+
+REM Check if the key exists: HKCR\.xxx\PersistentHandler
+call :SetErrorLevel 0
+reg query "%RegKeyHKCR%" >nul 2>&1
+if %ErrorLevel% neq 0 (
+    call :SetErrorLevel 0
+    reg add "!RegKeyHKCU!" /f /ve >nul
+    if !ErrorLevel! neq 0 (echo.Registry key: '!RegKeyHKCU!')1>&2 & goto ExitPause
+)
+
+REM Check if the "OriginalPersistentHandler" value exists at "HKCR\.xxx\PersistentHandler"
+reg query "%RegKeyHKCR%" /v "OriginalPersistentHandler" >nul 2>&1
+if %ErrorLevel% equ 0 set "OriginalPersistentHandlerExists=1"
+call :SetErrorLevel 0
+
+REM echo.DEBUG OriginalPersistentHandlerExists='%OriginalPersistentHandlerExists%'
+
+REM Get the default value at "HKCR\.xxx\PersistentHandler"
+for /f "tokens=2*" %%a in ('reg query "!RegKeyHKCR!" /ve 2^>nul') do set "CurrentPersistentHandler=%%b"
+call :SetErrorLevel 0
+REM echo.DEBUG CurrentPersistentHandler='%CurrentPersistentHandler%'
+
+REM Set the default value at "HKCU\Software\Classes\.xxx\PersistentHandler"
+reg add "%RegKeyHKCU%" /ve /d "%TextPersistentHandler%" /f >nul
+if %ErrorLevel% neq 0 (echo.Registry key: '!RegKeyHKCU!')1>&2 & goto ExitPause
+set ExitCode=0
+
+if "%CurrentPersistentHandler%" == "" goto ExitPause
+if /i "%CurrentPersistentHandler%" == "(value not set)" goto ExitPause
+if /i "%CurrentPersistentHandler%" == "%TextPersistentHandler%" goto ExitPause
+if %OriginalPersistentHandlerExists% neq 0 goto ExitPause
+
+set ExitCode=1
+reg add "%RegKeyHKCU%" /v "OriginalPersistentHandler" /d "%CurrentPersistentHandler%" /f >nul
+if %ErrorLevel% neq 0 (echo.Registry key: '!RegKeyHKCU!')1>&2 & goto ExitPause
+set ExitCode=0
+
+echo.Added plain text persistent handler for file extension: %Extension%
+
+goto ExitPause
+
+
+:Trim
+REM Trims leading and trailing whitespace.
+SetLocal EnableDelayedExpansion
+REM echo.DEBUG :Trim %*
+set Params=%*
+for /f "tokens=1*" %%a in ("!Params!") do EndLocal & set %1=%%b
+exit /b
+
+
+:SetErrorLevel
+REM echo.DEBUG :SetErrorLevel %*
+exit /b %1
+
+
+:TooManyArgs
+REM echo.DEBUG :TooManyArgs %*
+(echo.Too many arguments.)1>&2
+goto :Usage
+
+
+:BadArg
+REM echo.DEBUG :BadArg %*
+(echo.Invalid extension: "%Arg1NoQuotes%")1>&2
+exit /b 1
+
+
+:HelpArg
+REM echo.DEBUG :HelpArg %*
+
+
+:Usage
+call :PrintHeader
+echo.Usage:
+echo.  %ThisFileNameNoExt% [.]Extension
+echo.
+echo.    Extension    The name of the extension to add, optionally prefixed by ".".
+echo.
+echo.Examples:
+echo.  C:\^>%ThisFileName%
+echo.    Prompts for the file extension.
+echo.
+echo.  C:\^>%ThisFileName% "txt"
+echo.    Adds a plain text handler for the .txt file extension.
+echo.
+echo.  C:\^>%ThisFileName% ".txt"
+echo.    Adds a plain text handler for the .txt file extension.
+
+goto Exit
+
+
+:PrintHeader
+echo.
+echo.Description:
+echo.  Configures Windows Search to index the contents of all files with the specified
+echo.  extension as if they are plain text files.
+echo.
+exit /b 1
+
+
+:ExitPause
+REM Pause on error if this script was not run from a command line.
+if %ExitCode% equ 0 goto Exit
+set CmdCmdLineNoQuotes=%CmdCmdLine:"=%
+set CmdCmdLineNoFileName=!CmdCmdLineNoQuotes:%ThisFileName%=!
+if "%CmdCmdLineNoQuotes%" == "%CmdCmdLineNoFileName%" goto Exit
+REM echo.DEBUG :ExitPause ExitCode=%ExitCode%
+pause
+
+
+:Exit
+REM echo.DEBUG :Exit ExitCode=%ExitCode%
+@%ComSpec% /c exit %ExitCode% >nul
