2a8a06673ce9ed1af2bbb8e3203e705fcecffe4c
skataben <skataben@gmail.com>
2019-08-07 16:15:49 -0700

    Small optimization: skip some operations if new registry key. Save the original filter as {00000000-0000-0000-0000-000000000000} even if it is null or empty.

--- RegisterPlainTextFilterForFileExt.bat
+++ RegisterPlainTextFilterForFileExt.bat
@@ -85,8 +85,9 @@ goto RegisterFileExtension
 REM echo.DEBUG :RegisterFileExtension %*
 
 set RegKeyHKLM=HKLM\Software\Classes\%Extension%\PersistentHandler
-
 set TextPersistentHandler={5e941d80-bf96-11cd-b579-08002b30bfeb}
+set DefaultPersistentHandler={00000000-0000-0000-0000-000000000000}
+set PersistentHandlerKeyExists=1
 set OriginalPersistentHandlerExists=0
 set CurrentPersistentHandler=
 
@@ -97,38 +98,50 @@ call :SetErrorLevel 0
 reg query "%RegKeyHKLM%" >nul 2>&1
 if %ErrorLevel% neq 0 (
     call :SetErrorLevel 0
+    set "PersistentHandlerKeyExists=0"
     reg add "!RegKeyHKLM!" /f /ve >nul
     if !ErrorLevel! neq 0 echo>&2.Registry key: "!RegKeyHKLM!" & goto ExitPause
 )
 
+REM echo.DEBUG PersistentHandlerKeyExists='%PersistentHandlerKeyExists%'.
+if %PersistentHandlerKeyExists% equ 0 goto RegisterPersistentHandler
+
 REM Check if the "OriginalPersistentHandler" value exists.
 reg query "%RegKeyHKLM%" /v "OriginalPersistentHandler" >nul 2>&1
 if %ErrorLevel% equ 0 set "OriginalPersistentHandlerExists=1"
-call :SetErrorLevel 0
-
 REM echo.DEBUG OriginalPersistentHandlerExists='%OriginalPersistentHandlerExists%'
 
 REM Get the current PersistentHandler.
 for /f "tokens=2*" %%a in ('reg query "!RegKeyHKLM!" /ve 2^>nul') do set "CurrentPersistentHandler=%%b"
-call :SetErrorLevel 0
+REM echo.DEBUG CurrentPersistentHandler='%CurrentPersistentHandler%'
+
+if defined CurrentPersistentHandler (
+    if /i "!CurrentPersistentHandler!" == "(value not set)" set "CurrentPersistentHandler="
+)
+
+
+:RegisterPersistentHandler
+REM echo.DEBUG CurrentPersistentHandler='%CurrentPersistentHandler%'
+if not defined CurrentPersistentHandler (
+    set "CurrentPersistentHandler=%DefaultPersistentHandler%"
+)
 REM echo.DEBUG CurrentPersistentHandler='%CurrentPersistentHandler%'
 
 REM Display message and exit if there is already a plain text filter.
 if /i "!CurrentPersistentHandler!" == "!TextPersistentHandler!" (
     echo.Windows Search plain text filter already registered for file extension: !Extension!
-    set ExitCode=0
+    set "ExitCode=0"
     goto ExitPause
 )
 
 REM Set the new PersistentHandler.
+call :SetErrorLevel 0
 reg add "!RegKeyHKLM!" /ve /d "!TextPersistentHandler!" /f >nul
 if %ErrorLevel% neq 0 echo>&2.Registry key: "!RegKeyHKLM!" & goto ExitPause
 echo.Registered Windows Search plain text filter for file extension: !Extension!
 set ExitCode=0
 
 REM Save the old PersistentHandler if necessary.
-if not defined CurrentPersistentHandler goto ExitPause
-if /i "!CurrentPersistentHandler!" == "(value not set)" goto ExitPause
 if %OriginalPersistentHandlerExists% neq 0 goto ExitPause
 
 reg add "%RegKeyHKLM%" /v "OriginalPersistentHandler" /d "!CurrentPersistentHandler!" /f >nul
